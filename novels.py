70c841d7758dd73c6745d31bf3e2643d2f38e5e9
ruhi19 <93202363+ruhi19@users.noreply.github.com>
2022-01-21 17:00:48 +0530

    Update novels.py

--- novels.py
+++ novels.py
@@ -1,21 +1,8 @@
-from urllib.request import urlopen as uReq
-from bs4 import BeautifulSoup as soup
-
-my_url='https://entertainment.time.com/2005/10/16/all-time-100-novels/slide/all/'
-
-uClient=uReq(my_url)
-page_html = uClient.read()
-uClient.close()
-
-# html parser
-
-page_soup=soup(page_html,"html.parser")
-container=page_soup.findAll("ul",{"class":"items"})
-for x in range (1,9):
-	print(container[x].text)
-
-#for author, year, get button
-container1=page_soup.findAll("div",{"item selected"})
-for p in container1:
-	author_and_year = container1.findAll('p').text
-	print(author_and_year)
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+webD =  webdriver.Chrome("chromedriver.exe")
+webD.get('https://entertainment.time.com/2005/10/16/all-time-100-novels/slide/all/')
+n = webD.find_elements(By.CLASS_NAME, "item-list")
+for name in n:
+	print(name.text)
