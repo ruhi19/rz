249c9700fd18f16ba358125c9bf88cdde0c32fe7
ruhi19 <93202363+ruhi19@users.noreply.github.com>
2022-01-21 19:16:51 +0530

    Update novels.py

--- novels.py
+++ novels.py
@@ -6,3 +6,8 @@ webD.get('https://entertainment.time.com/2005/10/16/all-time-100-novels/slide/al
 n = webD.find_elements(By.CLASS_NAME, "item-list")
 for name in n:
 	print(name.text)
+lnks=webD.find_elements(By.TAG_NAME , "a")
+# traverse list
+for lnk in lnks:
+   # get_attribute() to get all href
+   print(lnk.get_attribute('href'))
