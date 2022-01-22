1684b3e571ad696dbbd6d602ad8399069164e307
ruhi19 <93202363+ruhi19@users.noreply.github.com>
2022-01-22 12:44:04 +0530

    Update novels.py

--- novels.py
+++ novels.py
@@ -3,11 +3,31 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 webD =  webdriver.Chrome("chromedriver.exe")
 webD.get('https://entertainment.time.com/2005/10/16/all-time-100-novels/slide/all/')
-n = webD.find_elements(By.CLASS_NAME, "item-list")
-for name in n:
-	print(name.text)
-lnks=webD.find_elements(By.TAG_NAME , "a")
-# traverse list
-for lnk in lnks:
-   # get_attribute() to get all href
-   print(lnk.get_attribute('href'))
+#list of classes containing novels
+class_list = webD.find_elements(By.CLASS_NAME, "items") 
+len_class_list = len(class_list)
+for i in range(1,9):
+	novels_details = class_list[i].find_elements(By.TAG_NAME, "li")
+	for novels in novels_details:
+		novel_name = novels.text
+		print(novel_name)
+		novel_link = novels.find_element(By.TAG_NAME, "a").get_attribute('href')
+		print(novel_link)
+		
+		# Open a new window
+		webD.execute_script("window.open('');")
+
+		# Switch to the new window and open new URL
+		webD.switch_to.window(webD.window_handles[1])
+		webD.get(novel_link)
+		novel_info = webD.find_element(By.CLASS_NAME, "post-rail")
+		novel_author_name = novel_info.find_elements(By.TAG_NAME, "p")[0]
+		novel_author_name = str(novel_author_name.text).split(":")[1]
+		novel_year_released = novel_info.find_elements(By.TAG_NAME, "p")[1]
+		novel_year_released = str(novel_year_released.text).split(":")[1]
+		novel_purchase_link = novel_info.find_element(By.TAG_NAME, "a").get_attribute('href')
+		print("Author name:" + novel_author_name)
+		print("Year Released:" + novel_year_released)
+		print("Purchase Link:" + novel_purchase_link)
+		webD.close()
+		webD.switch_to.window(webD.window_handles[0])
