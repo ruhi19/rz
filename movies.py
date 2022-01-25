bd9335c193381682e2cafd709675ae70e438bdfc
ruhi19 <93202363+ruhi19@users.noreply.github.com>
2022-01-25 13:24:49 +0530

    Create movies.py

---
+++ movies.py
@@ -0,0 +1,15 @@
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+import re
+import csv
+webD =  webdriver.Chrome("chromedriver.exe")
+webD.get('https://en.wikipedia.org/wiki/List_of_films_considered_the_best')
+enitre_page_data = webD.find_elements(By.TAG_NAME, "ul") 
+for i in range(5,84):
+		movie_names = enitre_page_data[i].find_elements(By.TAG_NAME, "li")
+		for movie_name in movie_names:
+			title = str(movie_name.text).split("(")[0]
+			print(title)
+			year = re.split('( | ,) |' , movie_name.text)[1]
+			print(year)
