c617403c896110ef3c1775edc6a8c3eb026c4a42
aayushimahajan <97786479+aayushimahajan@users.noreply.github.com>
2022-01-25 18:25:21 +0530

    Update movies.py

--- movies.py
+++ movies.py
@@ -6,7 +6,7 @@ import re
 webD =  webdriver.Chrome("chromedriver.exe")
 webD.get('https://en.wikipedia.org/wiki/List_of_films_considered_the_best')
 enitre_page_data = webD.find_elements(By.TAG_NAME, "ul") 
-for i in range(5,84):
+for i in range(5,8):
 		movie_names = enitre_page_data[i].find_elements(By.TAG_NAME, "li")
 		for movie_name in movie_names:
 			title = str(movie_name.text).split("(")[0]
