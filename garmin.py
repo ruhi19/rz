def0ed016bb6495d9a26f3349bd400927bc3a3d0
ruhi19 <93202363+ruhi19@users.noreply.github.com>
2022-02-11 19:52:36 +0530

    Update garmin.py

--- garmin.py
+++ garmin.py
@@ -3,10 +3,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 import csv
 import re
+import time
 webD =  webdriver.Chrome("chromedriver.exe")
-for page in range(1,3):
-	webD.get('https://www.amazon.in/s?i=sporting&bbn=4730577031&rh=n%3A4730577031%2Cp_89%3AGarmin&dc&qid=1644519013&rnid=3837712031&ref=sr_pg_' + str(page))	
-	for i in range(4,5):
+for page in range(1,2):
+	url = 'https://www.amazon.in/s?i=sporting&bbn=4730577031&rh=n%3A4730577031%2Cp_89%3AGarmin&dc&qid=1644519013&rnid=3837712031&ref=sr_pg_' + str(page)
+	print(url)
+	webD.get(url)	
+	for i in range(4,27):
+		print(i)
 		j=i-3
 		image_link = webD.find_elements(By.CLASS_NAME, "s-image")[j].get_attribute('src')
 		print(image_link)
@@ -22,38 +26,59 @@ for page in range(1,3):
 		print(product_title.text)
 		description = webD.find_element(By.ID , "feature-bullets")
 		print(description.text)
-		#table = webD.find_element(By.CLASS_NAME, "a-lineitem a-align-top")
-		#if(table.find_element(By.TAG_NAME, "td").text != "Deal Price:"):
-		#	final_price = webD.find_element(By.CLASS_NAME, "priceToPay")
-		#	print(final_price.text)
-		#else:
-		#	final_price = webD.find_element(By.CLASS_NAME, "apexPriceToPay")
-		#	print(final_price.text)
-		final_price = webD.find_element(By.CLASS_NAME, "priceToPay")
-		print(final_price.text)
-		actual_price = webD.find_element(By.CLASS_NAME, "basisPrice").text.split(":")[1]
-		print(actual_price)
-		discount = webD.find_element(By.CLASS_NAME, "savingsPercentage")
-		print(discount.text)
-		reviews_link = webD.find_element(By.ID , "reviews-medley-footer").find_element(By.TAG_NAME,"a").get_attribute('href')
-		print(reviews_link)
+		try:
+			reviews_link = webD.find_element(By.ID , "reviews-medley-footer").find_element(By.TAG_NAME,"a").get_attribute('href')
+			print(reviews_link)
+		except:
+			print("No reviews")
 		actual_rating = webD.find_element(By.CLASS_NAME , "averageStarRatingNumerical")
 		print(actual_rating.text)
 		total_rating = webD.find_element(By.CLASS_NAME, "averageStarRating")
 		print(total_rating.text)
 		try:
-			list_of_variations = webD.find_element(By.CLASS_NAME, "imageSwatches")
-			variations = list_of_variations.find_elements(By.TAG_NAME, "li")
-			for variation_info in range(0, len(variations))
-				variation_info = variations[k].find_elements(By.ID , "productOverview_feature_div")
-				colour = webD.variation_info.find_element(By.TAG_NAME, "tr")[3].find_element(By.TAG_NAME, "td")[1].text
-				print(colour)
-				price = list_of_variations.find_elements(By.CLASS_NAME, "twisterSwatchPrice")
-				print(price.text)
+			variations_div = webD.find_element(By.CLASS_NAME, "imageSwatches")
+			colour_variations = variations_div.find_elements(By.TAG_NAME, "li")
+			for colour in colour_variations:
+				colour.click()
+				time.sleep(2)
+				colour_div = webD.find_element(By.ID , "productOverview_feature_div").find_element(By.TAG_NAME, "table")
+				colour_div = colour_div.find_elements(By.TAG_NAME , "tr")
+				for c_div in colour_div:
+					tag_name = c_div.find_elements(By.TAG_NAME, "td")[0].text
+					if tag_name =="Colour":
+						colour_name = c_div.find_elements(By.TAG_NAME, "td")[1].text
+				print(colour_name)
+				price_div = webD.find_element(By.ID, "apex_desktop")
+				price_div = price_div.find_elements(By.CLASS_NAME, "a-price")
+				final_price = price_div[0].text.split("\n")[0]
+				print(final_price)
+				actual_price = price_div[1]
+				print(actual_price.text)
+				try:
+					discount = webD.find_element(By.CLASS_NAME, "savingsPercentage")
+				except:
+					continue
+				print(discount.text)
 		except:
-		#for case with no variations
-			colour = webD.find_elements(By.ID, "productOverview_feature_div").find_element(By.TAG_NAME, "tr")[3].find_element(By.TAG_NAME, "td")[1].text
-			print(colour)
+			print("Only one colour present")
+			colour_div = webD.find_element(By.ID , "productOverview_feature_div").find_element(By.TAG_NAME, "table")
+			colour_div = colour_div.find_elements(By.TAG_NAME , "tr")
+			for c_div in colour_div:
+				tag_name = c_div.find_elements(By.TAG_NAME, "td")[0].text
+				if tag_name =="Colour":
+					colour_name = c_div.find_elements(By.TAG_NAME, "td")[1].text
+			print(colour_name)
+			price_div = webD.find_element(By.ID, "apex_desktop")
+			price_div = price_div.find_elements(By.CLASS_NAME, "a-price")
+			#final_price = price_div[0].text.split("\n")[0]
+			#print(final_price)
+			actual_price = price_div[1]
+			print(actual_price.text)
+			try:
+				discount = webD.find_element(By.CLASS_NAME, "savingsPercentage")
+			except:
+				continue
+			print(discount.text)
 		print("----------------------------------------------------------------------------------------------------------------")
 		webD.close()
 		webD.switch_to.window(webD.window_handles[0])
